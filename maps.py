5962e0fcd67f6b8c1753773f890dc27d5d36792c
mugilan6379 <thiyagarajanaao@gmail.com>
2024-05-04 20:27:24 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -50,7 +50,7 @@ def mapsViz(main_file,league):
     m = create_map(gdf,league)
     col1,col2,col3=st.columns([1, 2, 1]) 
     with col2:
-         folium_static(m,width=800,height=600)
+         folium_static(m,width=800,height=200)
     #st.write(gdf)
 
     
